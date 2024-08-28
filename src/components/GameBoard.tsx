f33e97c2cc3ec127b498ffd2cf18380f37576fe9
jooaogbriel <joao.ggwp@outlook.com>
2024-08-28 20:56:17 -0300

    feat: feature to define player winner
     i add a tag p to show a winner for user

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,24 +1,9 @@
-import { GameBoardProps, PlayerSymbol } from "../types";
+import { GameBoardProps } from "../types";
 
-const initialGameBoard: PlayerSymbol[][] = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }: GameBoardProps) {
-  const gameBoard = initialGameBoard.map((row) => row.slice());
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+export default function GameBoard({ onSelectSquare, board }: GameBoardProps) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
